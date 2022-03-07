7d7fd7da65b38f4bb0f41b9d89b16fb43de8fd7d
pascaljuma <80028893+pascaljuma@users.noreply.github.com>
2022-03-07 12:29:08 +0300

    added new files

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          <h6>About Us</h6>
+          <ul>
+
+
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+            <li><a href="page.php?type=faqs">FAQs</a></li>
+            
+          <li><a href="page.php?type=terms">Terms of use</a></li>
+               <li><a href=" ">Admin Login</a></li>
+          </ul>
+        </div>
+            
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+          <div class="footer_widget">
+            <p>Follow us:</p>
+            <ul>
+              <!-- <li><a href="https://code-projects.org/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li> -->
+              <li><a href=" "><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <p class="copy-right">Copyright &copy; Taxihire @ 2022 by Pascal Juma </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ page1.php
@@ -0,0 +1,392 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>Taxi hire Portal | Page details</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+
+        
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+<body>
+  
+        
+<!--Header-->
+
+<header>
+  <div class="default-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-2">
+        </div>
+        <div class="col-sm-9 col-md-10">
+          <div class="header_info">
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">EMail us : </p>
+              <a href="mailto:taxihire@gmail.com">taxihire@gmail.com</a> </div>
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">Call Us: </p>
+              <a href="tel:61-1234-5678-09">+254-743229542</a> </div>
+            <div class="social-follow">
+              <ul>
+                <li><a href=" "><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+                <li><a href=" "><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+                <li><a href=" "><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+                <li><a href=" "><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+                <li><a href=" "><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              </ul>
+            </div>
+    <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Navigation -->
+  <nav id="navigation_bar" class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      </div>
+      <div class="header_wrap">
+        <div class="user_login">
+          <ul>
+            <!-- <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i> -->
+ 
+   <!-- <i class="fa fa-angle-down" aria-hidden="true"></i></a> -->
+              <ul class="dropdown-menu">
+                       <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Profile</a></li>
+              <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Update Password</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Bookings</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Post a Testimonial</a></li>
+          <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">My Testimonial</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Logout</a></li>
+                      </ul>
+            </li>
+          </ul>
+        </div>
+        <!-- <div class="header_search">
+          <div id="search_toggle"><i class="fa fa-search" aria-hidden="true"></i></div>
+          <form action="#" method="get" id="header-search-form">
+            <input type="text" placeholder="Search..." class="form-control">
+            <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+          </form>
+        </div> -->
+      </div>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a>    </li>
+
+          <li><a href="About-us.php">About Us</a></li>
+          <li><a href="car-showcase.php">Car Showcase</a>
+          <li><a href="page1.php">FAQs</a></li>
+          <li><a href="contact-us.php">Contact Us</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navigation end -->
+
+</header>
+                      <section class="page-header aboutus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>FAQs</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li>FAQs</li>
+      </ul>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+<section class="about_us section-padding">
+  <div class="container">
+    <div class="section-header text-center">
+
+
+      <h2>FAQs</h2>
+      <p>																														<span style="color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;">Frequently Asked Quiz... &nbsp; &nbsp;We got you</span> </p>
+    </div>
+     </div>
+</section>
+<!-- /About-us--> 
+
+
+
+
+
+<<!--Footer -->
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          <h6>About Us</h6>
+          <ul>
+
+
+          <li><a href="About-us.php">About Us</a></li>
+            <li><a href="page1.php">FAQs</a></li>
+            
+          <li><a href="page.php?type=terms">Terms of use</a></li>
+               <li><a href=" ">Admin Login</a></li>
+          </ul>
+        </div>
+            
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+          <div class="footer_widget">
+            <p>Follow us:</p>
+            <ul>
+              <!-- <li><a href="https://code-projects.org/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li> -->
+              <li><a href=" "><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+              <li><a href=" "><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <p class="copy-right">Copyright &copy; Taxihire @ 2022 by Pascal Juma </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+
+<div class="modal fade" id="loginform">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Login</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="login_wrap">
+            <div class="col-md-12 col-sm-6">
+              <form method="post">
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" placeholder="Email address*">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="password" placeholder="Password*">
+                </div>
+                <div class="form-group checkbox">
+                  <input type="checkbox" id="remember">
+               
+                </div>
+                <div class="form-group">
+                  <input type="submit" name="login" value="Login" class="btn btn-block">
+                </div>
+              </form>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer text-center">
+        <p>Don't have an account? <a href="#signupform" data-toggle="modal" data-dismiss="modal">Signup Here</a></p>
+        <p><a href="#forgotpassword" data-toggle="modal" data-dismiss="modal">Forgot Password ?</a></p>
+      </div>
+    </div>
+  </div>
+</div><!--/Login-Form --> 
+
+<!--Register-Form -->
+
+
+<script>
+function checkAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'emailid='+$("#emailid").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>
+<script type="text/javascript">
+function valid()
+{
+if(document.signup.password.value!= document.signup.confirmpassword.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.signup.confirmpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+<div class="modal fade" id="signupform">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Sign Up</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="signup_wrap">
+            <div class="col-md-12 col-sm-6">
+              <form  method="post" name="signup" onSubmit="return valid();">
+                <div class="form-group">
+                  <input type="text" class="form-control" name="fullname" placeholder="Full Name" required="required">
+                </div>
+                      <div class="form-group">
+                  <input type="text" class="form-control" name="mobileno" placeholder="Mobile Number" maxlength="10" required="required">
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control" name="emailid" id="emailid" onBlur="checkAvailability()" placeholder="Email Address" required="required">
+                   <span id="user-availability-status" style="font-size:12px;"></span> 
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="confirmpassword" placeholder="Confirm Password" required="required">
+                </div>
+                <div class="form-group checkbox">
+                  <input type="checkbox" id="terms_agree" required="required" checked="">
+                  <label for="terms_agree">I Agree with <a href="#">Terms and Conditions</a></label>
+                </div>
+                <div class="form-group">
+                  <input type="submit" value="Sign Up" name="signup" id="submit" class="btn btn-block">
+                </div>
+              </form>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer text-center">
+        <p>Already got an account? <a href="#loginform" data-toggle="modal" data-dismiss="modal">Login Here</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+  <script type="text/javascript">
+function valid()
+{
+if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value)
+{
+alert("New Password and Confirm Password Field do not match  !!");
+document.chngpwd.confirmpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+<div class="modal fade" id="forgotpassword">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title">Password Recovery</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="forgotpassword_wrap">
+            <div class="col-md-12">
+              <form name="chngpwd" method="post" onSubmit="return valid();">
+                <div class="form-group">
+                  <input type="email" name="email" class="form-control" placeholder="Your Email address*" required="">
+                </div>
+  <div class="form-group">
+                  <input type="text" name="mobile" class="form-control" placeholder="Your Reg. Mobile*" required="">
+                </div>
+  <div class="form-group">
+                  <input type="password" name="newpassword" class="form-control" placeholder="New Password*" required="">
+                </div>
+  <div class="form-group">
+                  <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password*" required="">
+                </div>
+                <div class="form-group">
+                  <input type="submit" value="Reset My Password" name="update" class="btn btn-block">
+                </div>
+              </form>
+              <div class="text-center">
+                <p class="gray_text">For security reasons we don't store your password. Your password will be reset and a new one will be send.</p>
+                <p><a href="#loginform" data-toggle="modal" data-dismiss="modal"><i class="fa fa-angle-double-left" aria-hidden="true"></i> Back to Login</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+
+</html>
